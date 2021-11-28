e9eb89b91b32c45d8e1c058b0abc00b9025c499e
Walter <walterjgsp@gmail.com>
2021-11-27 20:46:08 -0500

    Building proto using Gradle

---
+++ proto/build.gradle.kts
@@ -0,0 +1,57 @@
+import com.google.protobuf.gradle.*
+import org.gradle.kotlin.dsl.proto
+
+val protobufVersion = "3.17.1"
+val grpcVersion = "1.42.0"
+val grpcKotlinVersion = "1.2.0"
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+    id("com.google.protobuf") version "0.8.18"
+}
+
+group = "org.wcode.usermanagement"
+version = "1.0.0-SNAPSHOT"
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+    implementation("io.grpc:grpc-stub:1.40.1")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk7@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            task.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
+    }
+}
